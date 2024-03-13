0abb283774281ece785f6ccbea132f804039d447
여경현 <121675605+yeokyunghyun@users.noreply.github.com>
2024-03-13 14:45:12 +0900

    Crawler2
    Crawler2

--- src/main/java/com/example/Graduation/GraduationApplication.java
+++ src/main/java/com/example/Graduation/GraduationApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GraduationApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GraduationApplication.class, args);
 	}
---
+++ src/main/java/com/example/Graduation/crawler/Crawler2.java
@@ -0,0 +1,7 @@
+package com.example.Graduation.crawler;
+
+public class Crawler2 {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
